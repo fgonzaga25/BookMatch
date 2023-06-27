81a970a803aab02d560483ea0cd4bc3cb6578f26
Alécio <aleciofvjunior@gmail.com>
2023-06-27 20:08:40 -0300

    Sexto Commit

--- static/js/input.js
+++ static/js/input.js
@@ -1,12 +1,12 @@
 function input() {
     var nomeLivro = (document.querySelector(".bookInput").value).trim().replace(" ", "_");
     //console.log(nomeLivro)
-    //window.location.replace(window.location.href + "/" + nomeLivro);
+    //window.location.replace("http://127.0.0.1:5000/recommend/" + nomeLivro);
     window.location.href = "http://127.0.0.1:5000/recommend/" + nomeLivro
 }
 
-document.querySelector(".bookInput").addEventListener("keyup", (event) => {
-  if (event.key === "Enter") {
+function handleKeyPress(event) {
+  if (event.keyCode === 13) {
     input()
   }
-});
+}
